74c2af33346a1e9e727fa887ef1031fa58249e58
lolipopmari <mit.maria2019@yandex.ru>
2023-03-22 10:52:53 +0300

    init from lr 4 v10

---
+++ LR4/v8/script.js
@@ -0,0 +1,71 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            countVertex: 0,
+            startVertex: 1,
+            selectedTab: 0,
+            tabs: ['Матрица кратчайших путей', 'Остовные деревья'],
+            arrData: [],
+            flag: false,
+            tree: [],
+            matrixPath: []
+        }
+    },
+
+    methods: {
+        addDataInput: function() {
+            this.arrData = [];
+            
+            for(let i = 0; i < this.countVertex; ++i)
+            {
+                this.arrData.push([]);
+                for(let j = 0; j < this.countVertex; ++j)
+                {
+                    this.arrData[i][j] = 0;
+                }
+            }
+
+            this.flag = false;
+        },
+        getResult: function()
+        {
+            this.flag = true;
+            this.tree = [];
+            this.matrixPath = [];
+
+            for(let i = 0; i < this.countVertex; ++i)
+            {
+                for(let j = 0; j < this.countVertex; ++j)
+                {
+                    if(this.arrData[i][j] < 0){
+                        this.flag = false;
+                        break;
+                    }
+                }
+
+                if(!this.flag) break;
+            }
+
+            if(this.flag)
+            {                
+                axios
+                    .post('/LR4/getResult.php', {
+                        graph: this.arrData
+                    }
+                    )
+                    .then(response => {
+                        response.data.forEach(element => {
+                            this.tree.push(element.tree);
+                            this.matrixPath.push(element.path);
+                        });
+                    })
+                    .catch(error => console.log(error));
+            }
+        },
+        getString: function(obj) {
+            return Object.keys(obj).map(v => Number(v) + 1).join(', ');
+        }
+    }
+})
+
+app.mount("#app")
